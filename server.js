7939de9e9bb85c74806de9162e327356ab2a76fc
Emilyeme <emilyemersom395@gmail.com>
2026-04-07 14:06:26 -0700

    NEW

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
-const bodyParser = require("body-parser")
+
 const cookieParser = require("cookie-parser")
 
 /* ***********************
@@ -43,23 +43,16 @@ app.use(function (req, res, next) {
   res.locals.messages = require("express-messages")(req, res)
   next()
 })
-// Unit 4, Process Registration Activity
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+//  Process Registration Activity
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
-
-// Unit 4, Process Registration Activity
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-// Unit 5 Authentication cookie use
+//  Authentication cookie use
 app.use(cookieParser())
 
 /* ***********************
